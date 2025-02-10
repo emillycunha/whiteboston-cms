ab1e9fb6b31c25499f4450c0dac2eb749605cb78
emillycunha <emillyhumphress@gmail.com>
2025-02-09 18:24:39 -0600

    fix(tailwind): resolve Tailwind not applying styles

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,15 +4,14 @@ export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
   css: ["~/assets/css/tailwind.css"],
-  postcss: {
-    plugins: {
-      "@tailwindcss/postcss": {},
-      //autoprefixer: {},
-    },
-  },
   modules: ["@pinia/nuxt", "@nuxt/eslint"],
   nitro: {
     preset: "static",
+  },
+  postcss: {
+    plugins: {
+      "@tailwindcss/postcss": {},
+    },
   },
   app: {
     head: {
